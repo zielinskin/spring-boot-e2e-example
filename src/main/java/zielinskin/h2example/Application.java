5241a1f67209f72e145f7d7bb5043d483bd3c2c2
zielinskin <zielinskin@outlook.com>
2023-02-21 01:52:00 -0600

    adding in dockets

--- src/main/java/zielinskin/h2example/Application.java
+++ src/main/java/zielinskin/h2example/Application.java
@@ -1,10 +1,12 @@
 package zielinskin.h2example;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +24,20 @@ public class Application implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public GroupedOpenApi studentDocket() {
+        return GroupedOpenApi.builder()
+                .group("Students")
+                .pathsToMatch("/students/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi lectureDockets() {
+        return GroupedOpenApi.builder()
+                .group("Lectures")
+                .pathsToMatch("/lectures/**")
+                .build();
     }
 }
