24f7f03cd5539da2a76de20d2c48ef5d5f5ac2d3
zielinskin <zielinskin@outlook.com>
2024-08-03 17:58:52 -0400

    - adding spring security with in-memory users - removing old references to previous project name - removing csrf protections; OpenAPI, despite using the published fix, doesn't seem to correctly work with CSRF protections

--- src/main/java/zielinskin/h2example/Application.java
+++ src/main/java/zielinskin/h2example/Application.java
@@ -5,13 +5,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 @EntityScan(basePackages={"zielinskin.h2example.data"})
 @EnableJpaRepositories(basePackages = {"zielinskin.h2example.data"})
+@EnableWebSecurity
 public class Application implements WebMvcConfigurer {
 
     @Override
@@ -22,6 +46,79 @@ public class Application implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(PasswordEncoder encoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User.builder()
+                .passwordEncoder(encoder::encode)
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build());
+        manager.createUser(User.builder()
+                .passwordEncoder(encoder::encode)
+                .username("admin")
+                .password("alsoAdmin")
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /*@Bean
+    UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.builder()
+                .passwordEncoder(new BCryptPasswordEncoder()::encode)
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .passwordEncoder(new BCryptPasswordEncoder()::encode)
+                .username("admin")
+                .password("alsoAdmin")
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
+    }*/
+
+
+    @Bean
+    public SecurityFilterChain formLoginFilterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login")
+                        .permitAll()
+                        .anyRequest()
+                        .hasRole("USER")
+                        .anyRequest()
+                        .permitAll()
+                )
+                .userDetailsService(userDetailsService)
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserDetailsManager userDetailsService,
+                                                            PasswordEncoder encoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder);
+        return authProvider;
+    }
+
 
     @Bean
     public GroupedOpenApi studentDocket() {
